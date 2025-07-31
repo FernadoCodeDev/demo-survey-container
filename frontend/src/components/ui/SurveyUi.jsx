6e872d5126b64059b0f820ae0ffafd41d8a9d9af
Fernando <fernando.e.mejia.e@gmail.com>
2025-07-31 09:37:28 -0600

    react-toastify and new prop

--- frontend/src/components/ui/SurveyUi.jsx
+++ frontend/src/components/ui/SurveyUi.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { SurveyWidget } from "survey-container";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function SurveyUi() {
   const { surveyId } = useParams();
 
   const apiUrl = `http://localhost:3000/api/surveys`;
+
 
   return (
     <div className="p-4">
@@ -14,6 +18,22 @@ function SurveyUi() {
         surveyId={surveyId}
         fetchUrl="http://localhost:3000/api/surveys/survey.php?id="
         responseUrl="http://localhost:3000/api/response/postResponse.php"
+        onAlert={(msg, type = "info") => {
+          toast(msg, {
+            type,
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          });
+        }}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
       />
     </div>
   );
